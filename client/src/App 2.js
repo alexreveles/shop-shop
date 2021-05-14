2874686010fde6b0b7934c764839b0dd8f508c29
alex <alexreveles@yahooo.com>
2021-05-14 14:11:48 -0700

    process payment added

--- client/src/App 2.js
+++ client/src/App 2.js
@@ -9,8 +9,6 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import { StoreProvider } from "./utils/GlobalState";
-import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
@@ -30,21 +28,19 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/success" component={Success} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route component={NoMatch} />
-            </Switch>
-          </StoreProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/orderHistory" component={OrderHistory} />
+            <Route exact path="/products/:id" component={Detail} />
+            <Route component={NoMatch} />
+          </Switch>
         </div>
       </Router>
     </ApolloProvider>
+
   );
 }
 
